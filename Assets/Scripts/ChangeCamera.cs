9569b4bf6764f160f432cb62e8a36a769e861a5c
Daniel <danielttgb44@live.com.mx>
2021-11-28 17:06:28 -0600

    Camaras en coches y cambio entre ellas

--- Assets/Scripts/ChangeCamera.cs
+++ Assets/Scripts/ChangeCamera.cs
@@ -2,36 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
+using System.Linq;
 public class ChangeCamera : MonoBehaviour
 {
-    // Lista de camaras
-    List<GameObject> cameraList;
-    public GameObject StreetCamera_1;
-    public GameObject StreetCamera_2;
-    public GameObject StreetCamera_3;
-    public GameObject StreetCamera_4;
-    public GameObject CenitalCamera;
+    // Lista de camaras vista
+    private GameObject[] viewCamerasList;
+    private int indexCameraViewList = 0;
 
     //Elementos del Canvas
     public Text textCurrentCamera;
     public Button buttonChangeCamera;
+    public InputField inputCar;
+    
 
-    private int indexCameraList = 0;
+    // Cars
+    private GameObject[] carsCamerasList;
+    private int indexCameraCarList = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        cameraList = new List<GameObject>();
-        cameraList.Add(StreetCamera_1);
-        cameraList.Add(StreetCamera_2);
-        cameraList.Add(StreetCamera_3);
-        cameraList.Add(StreetCamera_4);
-        cameraList.Add(CenitalCamera);
-        foreach (GameObject camera in cameraList) {
-            camera.SetActive(false);
-        }
-        cameraList[indexCameraList].SetActive(true);
-        textCurrentCamera.text = cameraList[indexCameraList].name;
+        if (viewCamerasList == null)                    { viewCamerasList = GameObject.FindGameObjectsWithTag("ViewCamera"); }
+        if (carsCamerasList == null)                    { carsCamerasList = GameObject.FindGameObjectsWithTag("Car").OrderBy( car => car.name ).ToArray();;  }
+
+        foreach (GameObject camera in carsCamerasList)  { camera.gameObject.transform.GetChild(0).gameObject.SetActive(false); }
+        foreach (GameObject camera in viewCamerasList)  { camera.SetActive(false); }
+        viewCamerasList[indexCameraViewList].SetActive(true);
+        textCurrentCamera.text = viewCamerasList[indexCameraViewList].name;
     }
 
     // Update is called once per frame
@@ -40,17 +38,28 @@ public class ChangeCamera : MonoBehaviour
         
     }
 
-    public void ChangeNextCamera()
+    public void ChangeNextView()
     {
-        indexCameraList += 1;
-        if (indexCameraList == cameraList.Count) {
-            indexCameraList = 0;
-        }
+        indexCameraViewList += 1;
+        if (indexCameraViewList == viewCamerasList.Length)   { indexCameraViewList = 0; }
         
-        foreach (GameObject camera in cameraList) {
-            camera.SetActive(false);
-        }
-        cameraList[indexCameraList].SetActive(true);
-        textCurrentCamera.text = cameraList[indexCameraList].name;
+        foreach (GameObject camera in carsCamerasList)  { camera.gameObject.transform.GetChild(0).gameObject.SetActive(false); }
+        foreach (GameObject camera in viewCamerasList)  { camera.SetActive(false); }
+        viewCamerasList[indexCameraViewList].SetActive(true);
+        textCurrentCamera.text = viewCamerasList[indexCameraViewList].name;
     }
+
+    public void ChangeNextCar()
+    {
+        if ((Int32.Parse(inputCar.text) - 1) < 0 || (Int32.Parse(inputCar.text)) > carsCamerasList.Length) {
+            textCurrentCamera.text = "Ingrese un número válido de coche";
+        } else {
+            foreach (GameObject camera in carsCamerasList)  { camera.gameObject.transform.GetChild(0).gameObject.SetActive(false); }
+            foreach (GameObject camera in viewCamerasList)  { camera.SetActive(false); }
+            carsCamerasList[Int32.Parse(inputCar.text) - 1].gameObject.transform.GetChild(0).gameObject.SetActive(true);
+            textCurrentCamera.text = carsCamerasList[Int32.Parse(inputCar.text) - 1].name;
+        }
+
+    }
+
 }
